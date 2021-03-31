80258994ef846fbaf6434c3f6e000856a333cfcd
Ming Nee <mingneeyap@gmail.com>
2021-03-31 21:37:32 +0800

    main page and room services page

--- app/src/main/java/com/example/mobilehotelmanagementsystem/mainpage.kt
+++ app/src/main/java/com/example/mobilehotelmanagementsystem/mainpage.kt
@@ -1,11 +1,22 @@
 package com.example.mobilehotelmanagementsystem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.view.View
 
 class mainpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainpage)
+    }
+    fun roomServices(view : View){
+        val greeting = "Room Services"
+
+        val intent = Intent(this, room_services::class.java).apply {
+            putExtra(AlarmClock.EXTRA_MESSAGE,greeting)
+        }
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/mobilehotelmanagementsystem/room_services.kt
@@ -0,0 +1,15 @@
+package com.example.mobilehotelmanagementsystem
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock
+import android.view.View
+
+class room_services : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_services)
+    }
+
+}
